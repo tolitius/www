e8e93ae3babb85c9dbb6ba2fd9d55d7eb384fbf6
anatoly <tolitius@gmail.com>
2014-03-05 23:15:54 -0500

    [0.1.1] to [0.1.2]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cccp/lein-template "0.1.1"
+(defproject cccp/lein-template "0.1.2"
   :description "Creates a new Clojure + ClojureScript + Compojure + Ring project template"
   :url "https://github.com/tolitius/cccp"
   :license {:name "Eclipse Public License"
